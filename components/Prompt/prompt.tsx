ae105643fc813aa80a79eb207f41b2f11a20fafd
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-02-18 03:23:21 +0000

    hook useTypedText

--- components/Prompt/prompt.tsx
+++ components/Prompt/prompt.tsx
@@ -1,15 +1,16 @@
-import { useMemo } from 'react'
+'use client'
+import { useEffect, useMemo, useState } from 'react'
 import styles from './prompt.module.css'
 import Token from './token'
 
 interface PromptProps {
   text: string
-  typedText: string
 }
 
-export default function Prompt ({ text, typedText = 'useMemo es ua' }: PromptProps): JSX.Element {
-  const tokens = useTokens(text)
-  const typedTokens = useTokens(typedText)
+export default function Prompt ({ text }: PromptProps): JSX.Element {
+  const { typedText } = useTypedText()
+  const [tokens] = useTokens(text)
+  const [typedTokens] = useTokens(typedText)
 
   return (
     <div className={styles.prompt}>
@@ -22,12 +23,40 @@ export default function Prompt ({ text, typedText = 'useMemo es ua' }: PromptPro
   )
 }
 
-function useTokens (text: string): string[] {
-  return useMemo(() => {
-    const tokens = text.split(' ')
-    return tokens.map((token, i) => {
-      const isLast = i === text.split(' ').length - 1
-      return isLast ? token : `${token} `
+function useTokens (text: string): [string[]] {
+  const tokens = useMemo(() => {
+    const splitedText = text.split(' ')
+    const tokens = splitedText.map((word, i) => {
+      const isLast = i === splitedText.length - 1
+      return isLast ? word : `${word} `
     })
+    return tokens
   }, [text])
+
+  return [tokens]
+}
+
+function useTypedText (): { typedText: string } {
+  const [typedText, setTypedText] = useState<string>('')
+
+  useEffect(() => {
+    const handleKeydown = (event: KeyboardEvent): void => {
+      const isChar = event.key.length === 1
+      const isBackspace = event.key === 'Backspace'
+      if (isChar) {
+        setTypedText(typedText => typedText + event.key)
+      }
+      if (isBackspace) {
+        setTypedText(typedText => typedText.slice(0, -1))
+      }
+    }
+
+    window.addEventListener('keydown', handleKeydown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown)
+    }
+  }, [])
+
+  return { typedText }
 }
